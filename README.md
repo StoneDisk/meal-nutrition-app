885a0bbbffbd2631d9ec9b4a718884adab3a0320
Francis Yvan Jubera <francisyvanjubera@gmail.com>
2023-04-05 09:57:49 +0800

    started working on getMealInfos() function in app.js and edited README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # meal-nutrition-app
-A web app that shows nutritional data of a chosen meal
+A web app that shows nutritional data of a chosen meal. It uses the spoonacular API for the meal information.
--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
-// Reminder: Supply a valid API key or this API request will not work!
+// Reminder: Supply a valid API key or the API requests will not work!
+
 async function getMeal() {
     const customHeader = new Headers();
     // This is where you set the api key
-    const apiKey = "";
+    const apiKey = "8d5bc60034c841b49d1476a3e39b9c6f";
     customHeader.append("x-api-key", apiKey);
 
-    const mealID = [];
-    let query = "corn dog";
+    const mealIDs = [];
+    let query = "burger";
     let dish_type = "snack";
     let number = "3";
     const url = "https://api.spoonacular.com/recipes/complexSearch?" + "query="
@@ -15,17 +16,44 @@ async function getMeal() {
     const response = await fetch(url, {
         headers: customHeader
     });
+
     const meals = await response.json();
     meals.results.forEach(result => {
-        mealID.push(result.id);
+        mealIDs.push(result.id);
     });
 
-    console.log(meals);
-    return mealID;
+    console.log(meals); // Remove this line when getMeal has been fully tested
+    return mealIDs;
 }
 
-async function getMealURL(mealID) {
-    
+function getMealInfos(mealIDs) {
+    const customHeader = new Headers();
+    // This is where you set the api key
+    const apiKey = "8d5bc60034c841b49d1476a3e39b9c6f";
+    customHeader.append("x-api-key", apiKey);
+
+    let url = "";
+    let response = null;
+    const mealInfos = [];
+    let mealInfo = "";
+
+
+    for (const mealID of mealIDs) {
+        url = `https://api.spoonacular.com/recipes/${mealID}/information?includeNutrition=false`;
+        // This setTimeout is needed to avoid exceeding the request rate limit of spoonacular API
+        // by delaying the next request to 3 seconds
+        setTimeout(async() => {
+            response = await fetch(url, {
+                headers: customHeader
+            });
+            mealInfo = await response.json(); 
+            mealInfos.push(mealInfo);
+        }, 3000);
+    }
+
+    return mealInfos;
 }
 
-// getMeal();
+/* getMeal().then((mealIDs) => {
+    console.log(mealIDs);
+}); */
